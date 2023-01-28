8324690d5dc37146bbe700cc0961e3774fde60ea
Matheus <ttmath@disroot.org>
2023-01-27 23:58:57 -0300

    added clients manager module

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,12 +1,24 @@
 class ClientsController < ApplicationController
   def index
+    @client = Client.all
   end
 
   def new
+    @client = Client.new
+    
   end
 
   def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      redirect_to @client, notice: 'Cliente cadastrado com sucesso!'
+    else
+      render :new
+    end
+    
   end
+  
 
   def edit
   end
@@ -15,5 +27,20 @@ class ClientsController < ApplicationController
   end
 
   def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
   end
+
+  private
+
+  def client_params
+    params.require(:client).permit(
+    :name,
+    :email,
+    :address,
+    :phone
+    )
+  end
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'clients/index'
-  get 'clients/new'
-  get 'clients/create'
-  get 'clients/edit'
-  get 'clients/update'
-  get 'clients/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ resources :clients
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
