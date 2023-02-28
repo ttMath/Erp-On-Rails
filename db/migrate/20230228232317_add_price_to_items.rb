bf67469bec4ef10d0218215443cb9debb02fce26
Matheus <tt.math@disroot.org>
2023-02-28 20:25:39 -0300

    added precision and scale params in migration

--- db/migrate/20230228232317_add_price_to_items.rb
+++ db/migrate/20230228232317_add_price_to_items.rb
@@ -1,5 +1,5 @@
 class AddPriceToItems < ActiveRecord::Migration[7.0]
   def change
-    add_column :items, :price, :decimal
+    add_column :items, :price, :decimal, :precision => 8, :scale => 2
   end
 end
