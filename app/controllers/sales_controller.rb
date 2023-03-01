e8c8e1d3125eefc68103f3bd33268fb3d4a11703
Matheus <tt.math@disroot.org>
2023-02-28 22:21:33 -0300

    configurated sales controller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,22 @@
 class SalesController < ApplicationController
+
+    def index
+        @sale = Sale.all
+    end
+
+    def show
+        @sale = Sale.find(id)
+    end
+    
+
+    private
+
+    def sales_params
+        params.require(:sale).permit(
+            :customer_id,
+            items_sale_attributes: [:id, :item_id, :quantity,un_price]
+            )
+    end
+    
+    
 end
