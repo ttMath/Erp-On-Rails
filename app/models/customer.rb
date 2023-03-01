6aef402e2b444e120ce2a1e098eebc68834ef53f
Matheus <tt.math@disroot.org>
2023-02-28 21:59:47 -0300

    edited models adding params

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,7 @@ class Customer < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true
     validates :phone, length: { maximum:11 }
+    has_many: sales
     
     
     
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
     validates :name, presence: true
     validates :stock, presence: true
     validates :price, presence: true
+    has_many :items_sales
 end
